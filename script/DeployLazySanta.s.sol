49268693439744bfee2d9878e9977063334e4242
chibyke <chibykepride@gmail.com>
2023-12-25 15:47:11 +0000

    initial commit. initial deployment. cast wallet utilized to deploy.

---
+++ script/DeployLazySanta.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {LazySanta} from "../src/LazySanta.sol";
+
+contract DeployLazySanta is Script {
+    function run() external returns(LazySanta) {
+        vm.startBroadcast();
+        LazySanta lazySanta = new LazySanta(msg.sender);
+        vm.stopBroadcast;
+        return lazySanta;
+    }
+}
--- src/LazySanta.sol
+++ src/LazySanta.sol
@@ -6,6 +6,8 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title LazySanta contract
+/// @notice This contract is for a token called LazySanta
 contract LazySanta is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
 
     // private variable to keep track of the next token Id
@@ -29,6 +31,11 @@ contract LazySanta is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     // mapping to keep track of number of mints each wallet has done
     mapping(address => uint256) public lazyWallets;
 
+    // Event emitted when a LazySanta is minted
+    event LazySantaMinted(address indexed minter, uint256 tokenId);
+
+    /// @notice Deploy a new LazySanta contract
+    /// @param chiefElf The address of the chief elf
     constructor(address chiefElf)
         ERC721("LazySanta", "LS")
         Ownable(chiefElf)
@@ -40,27 +47,52 @@ contract LazySanta is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         maxSupply = 20;
 
         // initialize lazyFee_ in constructor
-        lazyFee = 0.01 ether;
+        lazyFee = 0.01 * 1e18;
 
         // initialize uri in constructor
         uri = "ipfs://QmWj3UTHGEkcP2uHzk6wP4gzYQUKmKTZ4sVWLyiB7y9md7";
     }
 
-    // function to set the mint price
-    function setlazyFee_(uint256 lazyFee_) external onlyOwner {
-        lazyFee = lazyFee_;
+    // struct to hold the proposed changes
+    struct ProposedChange {
+        uint256 lazyFee_;
+        uint256 maxSupply_;
+        string uri_;
+        bool executed;
     }
 
-    // function to set the maximum supply
-    function setMaxSupply(uint256 maxSupply_) external onlyOwner {
-        maxSupply = maxSupply_;
+    // mapping to store proposed changes
+    mapping (address => ProposedChange) public proposedChanges;
+
+    /// @notice Propose a change to the lazyFee
+    /// @param lazyFee_ The proposed new lazyFee
+    function proposeSetLazyFee(uint256 lazyFee_) external onlyOwner {
+        proposedChanges[msg.sender] = ProposedChange(lazyFee_, maxSupply, uri, false);
     }
 
-    // function to set the uri
-    function setUri(string memory uri_) external onlyOwner {
-        uri = uri_;
+    /// @notice Propose a change to the maxSupply
+    /// @param maxSupply_ The proposed new maxSupply
+    function proposeSetMaxSupply(uint256 maxSupply_) external onlyOwner {
+        proposedChanges[msg.sender] = ProposedChange(lazyFee, maxSupply_, uri, false);
     }
 
+    /// @notice Propose a change to the uri
+    /// @param uri_ The proposed new uri
+    function proposeSetUri(string memory uri_) external onlyOwner {
+        proposedChanges[msg.sender] = ProposedChange(lazyFee, maxSupply, uri_, false);
+    }
+
+    /// @notice Execute proposed changes
+    function executeProposedChanges() external onlyOwner {
+        require(!proposedChanges[msg.sender].executed, "Changes already executed!");
+        lazyFee = proposedChanges[msg.sender].lazyFee_;
+        maxSupply = proposedChanges[msg.sender].maxSupply_;
+        uri = proposedChanges[msg.sender].uri_;
+        proposedChanges[msg.sender].executed = true;
+    }
+
+    /// @notice Safely mint a LazySanta
+    /// @dev Requires payment of the correct amount of ether and checks that the maximum supply has not been reached
     function safeMint() payable external {
         require(lazyWallets[msg.sender] < 1, "Max LazySanta mint per wallet exceeded!");
         require(msg.value == lazyFee, "Input accurate price of one LazySanta!");
@@ -73,17 +105,33 @@ contract LazySanta is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         uint256 tokenId = currentSupply;
         _safeMint(msg.sender, tokenId);
         _setTokenURI(tokenId, uri);
+        nextTokenId_++;
+
+        emit LazySantaMinted(msg.sender, tokenId);
     }
 
+    /// @notice Function to withdraw all funds from the contract
+    /// @dev This function can only be called by the owner of the contract
     function withdraw() external onlyOwner {
-         for(uint256 lazyListIndex = 0; lazyListIndex < lazyList.length; lazyListIndex++) {
+        // Reset the count of mints for each wallet
+        for (uint256 lazyListIndex = 0; lazyListIndex < lazyList.length; lazyListIndex++) {
             address lazyMinter = lazyList[lazyListIndex];
             lazyWallets[lazyMinter] = 0;
         }
+        // Clear the list of lazy wallets
         lazyList = new address[](0);
-        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
-        require(callSuccess, "Call failed!");
+
+        // Get the balance of the contract
+        uint256 contractBalance = address(this).balance;
+        // Check if there are funds to withdraw
+        require(contractBalance > 0, "No funds to withdraw");
+
+        // Transfer funds before updating state variables to avoid reentrancy
+        (bool callSuccess,) = payable(msg.sender).call{value: contractBalance}("");
+        // Check if the transfer was successful
+        require(callSuccess, "Withdrawal failed!");
     }
+
 
     // function to get all minters
     function getLazyList() external view onlyOwner returns(address[] memory) {
